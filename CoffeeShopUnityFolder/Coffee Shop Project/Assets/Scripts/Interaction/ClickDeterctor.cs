6ac5755819ff8bb80ee0270f019fb81811297d49
RaulTibuleac <raul.tibuleac@gmail.com>
2026-09-17 16:49:54 +0100

    feat: Improved clicker
    Improved the clicking function

--- CoffeeShopUnityFolder/Coffee Shop Project/Assets/Scripts/Interaction/ClickDeterctor.cs
+++ CoffeeShopUnityFolder/Coffee Shop Project/Assets/Scripts/Interaction/ClickDeterctor.cs
@@ -1,13 +1,22 @@
+//THIS IS SCRIPT A
 using UnityEngine;
 
 public class ClickDetector : MonoBehaviour
 {
-    [SerializeField] private int CountClicks;
-
-    private void OnMouseDown()
+    //Every frame
+    private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //This Is tagged "Main Camera" on the deafuult camera - only one camera should be the "Main Camera"
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                IClickable clickable = hit.collider.GetComponent<IClickable>();
 
-        Debug.Log("Hello, Oscar");
-        CountClicks++;
+                clickable?.OnClick();
+                //calls the interface "IClickable"
+            }
+        }
     }
 }
